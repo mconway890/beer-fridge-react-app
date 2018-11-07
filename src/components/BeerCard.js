8b575aa2619783d3bd403065843061cff8c86eee
Michele Conway <mconway890@gmail.com>
2018-11-07 13:17:29 -0500

    Add elements in beer card to Table

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import {Button, ButtonGroup} from 'react-bootstrap';
+import {Button, ButtonGroup, Table} from 'react-bootstrap';
 
 const BeerCard = ({ beer, removeBeer, upvoteBeer, downvoteBeer }) =>
 			<div>
-				<a>
-					<h3 className="card-title">{beer.name}</h3>
-				</a>
-				<p>{beer.style} By {beer.breweryName}</p>
-				<ButtonGroup bsSize="small">
-					<Button bsStyle="success" onClick={() => upvoteBeer(beer.id)}>Upvote</Button>
-					<Button bsStyle="warning" onClick={() => downvoteBeer(beer.id)}>Downvote</Button>
-					<Button bsStyle="danger" onClick={() => removeBeer(beer.id)}><span aria-hidden="true">&times;</span></Button>
-				</ButtonGroup>
-				<div>Votes: {beer.votes}</div>
+				<Table striped bordered condensed hover>
+			  <thead>
+			    <tr>
+			      <th>Beer Name</th>
+			      <th>Style</th>
+			      <th>Brewery</th>
+						<th>Rate</th>
+						<th>Rating</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    <tr>
+			      <td>{beer.name}</td>
+			      <td>{beer.style}</td>
+			      <td>{beer.breweryName}</td>
+						<td>
+						<ButtonGroup bsSize="small">
+							<Button bsStyle="success" onClick={() => upvoteBeer(beer.id)}>Upvote</Button>
+							<Button bsStyle="warning" onClick={() => downvoteBeer(beer.id)}>Downvote</Button>
+							<Button bsStyle="danger" onClick={() => removeBeer(beer.id)}><span aria-hidden="true">&times;</span></Button>
+						</ButtonGroup>
+						</td>
+						<td>Rating: {beer.votes}</td>
+			    </tr>
+					</tbody>
+				</Table>
 		  </div>
 		;
 

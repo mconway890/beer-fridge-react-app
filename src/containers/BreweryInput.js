3690d42ab97389bc0cc2039dba2a1b8ea858bf6a
Michele Conway <mconway890@gmail.com>
2018-11-07 10:35:33 -0500

    Change input type to button, add comments

--- src/containers/BreweryInput.js
+++ src/containers/BreweryInput.js
@@ -6,10 +6,12 @@ import { connect } from 'react-redux';
 export class BreweryInput extends Component {
 
   state = {
+    // set up controlled form with internal state
     breweryName: '',
   }
 
   handleOnChange = event => {
+    // handle updating component state
     this.setState({
       [event.target.name]: event.target.value
     });
@@ -17,8 +19,11 @@ export class BreweryInput extends Component {
 
 
   handleOnSubmit = event => {
+    // handle form submit
     event.preventDefault();
+    // create beer object from state
     const brewery = {...this.state, id: uuid() };
+    // pass beer object to action creator
     this.props.addBrewery(brewery);
     this.setState({
       breweryName: ''
@@ -38,11 +43,12 @@ export class BreweryInput extends Component {
                 value={this.state.breweryName}
                 placeholder="brewery name" />
             </p>
-            <input type="submit" />
+            <button type="submit">Add Brewery</button>
           </form>
         </div>
     );
   }
 };
 
+// add arguments to connect as needed
 export default connect(null, { addBrewery })(BreweryInput);

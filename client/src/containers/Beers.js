eae6565f24457ef4efea693a28aa50320def42e9
Michele Conway <mconway890@gmail.com>
2018-11-09 13:15:50 -0500

    Remove uneeded imports

--- client/src/containers/Beers.js
+++ client/src/containers/Beers.js
@@ -3,27 +3,22 @@ import { connect } from 'react-redux';
 import '../App.css';
 import BeerCard from '../components/BeerCard';
 import { removeBeer, upvoteBeer, downvoteBeer, getBeers } from '../actions/beerActions';
-import { Route } from 'react-router-dom';
-import DisplayBeer from './DisplayBeer';
 import BeerInput from './BeerInput';
 
 
 class Beers extends Component {
 
   render() {
-    const beers = this.props.beers
+    //const beers = this.props.beers
 
     return (
       <div>
         <BeerInput />
             <div className="BeersContainer">
               <h1 className="beerName">Beers</h1>
-                {beers.map(beer =>
+                {this.props && this.props.beers.map(beer =>
                 <BeerCard
                 key={beer.id}
-                upvoteBeer={upvoteBeer}
-                downvoteBeer={downvoteBeer}
-                removeBeer={removeBeer}
                 beer={beer} /> )}
           </div>
       </div>

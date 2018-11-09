1cf9f7998f9012c8fd8e6846058cf414453b0c23
Michele Conway <mconway890@gmail.com>
2018-11-09 11:14:46 -0500

    Add class to module API

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,31 +1,33 @@
 module Api::V1
-  before_action :set_beer, only: [:show, :update, :destroy]
+  class BeersController < ApplicationController
+    before_action :set_beer, only: [:show, :update, :destroy]
 
-  def index
-    @beers = Beer.all
-    render json: @beers
-  end
+    def index
+      @beers = Beer.all
+      render json: @beers
+    end
 
-  def create
-    @beer = Beer.new(beer_params)
-    if @beer.save
+    def create
+      @beer = Beer.new(beer_params)
+      if @beer.save
+        render json: @beer
+      else
+        render json: {message: beer.errors}, status: 400
+      end
+    end
+
+    def show
       render json: @beer
-    else
-      render json: {message: beer.errors}, status: 400
     end
+
+    private
+
+      def set_beer
+        @beer = Beer.find_by(id: params[:id])
+      end
+
+      def beer_params
+        params.require(:beer).permit(:name, :style, :brewery, :brewery_id)
+      end
   end
-
-  def show
-    render json: @beer
-  end
-
-  private
-
-    def set_beer
-      @beer = Beer.find_by(id: params[:id])
-    end
-
-    def beer_params
-      params.require(:beer).permit(:name, :style, :brewery, :brewery_id)
-    end
 end

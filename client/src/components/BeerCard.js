7d42ae95350596e80c4d19ae135185dfe225859b
Michele Conway <mconway890@gmail.com>
2018-11-09 13:08:54 -0500

    Beer card renders beer table

--- client/src/components/BeerCard.js
+++ client/src/components/BeerCard.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import {Button, ButtonGroup, Table} from 'react-bootstrap';
+import {Table} from 'react-bootstrap';
 
-const BeerCard = ({ beer, removeBeer, upvoteBeer, downvoteBeer }) =>
+const BeerCard = ({ beer }) =>
 			<div>
 				<Table striped bordered condensed hover>
 			  <thead>
 			    <tr>
 			      <th>Beer Name</th>
 			      <th>Style</th>
-			      <th>Brewery</th>
-						<th>Rate</th>
-						<th>Rating</th>
 			    </tr>
 			  </thead>
 			  <tbody>
 			    <tr>
-			      <td>{beer.name}</td>
+			      <td><a href={`beers/${beer.id}`}>{beer.name}</a></td>
 			      <td>{beer.style}</td>
-			      <td>{beer.breweryName}</td>
-						<td>
-						<ButtonGroup bsSize="small">
-							<Button bsStyle="success" onClick={() => upvoteBeer(beer.id)}>Upvote</Button>
-							<Button bsStyle="warning" onClick={() => downvoteBeer(beer.id)}>Downvote</Button>
-							<Button bsStyle="danger" onClick={() => removeBeer(beer.id)}><span aria-hidden="true">&times;</span></Button>
-						</ButtonGroup>
-						</td>
-						<td>Rating: {beer.votes}</td>
 			    </tr>
 					</tbody>
 				</Table>

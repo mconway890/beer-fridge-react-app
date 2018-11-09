91bf072fd008acca2d14f302e74ba6b8bcb1f1d5
Michele Conway <mconway890@gmail.com>
2018-11-09 11:54:16 -0500

    Add form to beers.js

--- client/src/containers/Beers.js
+++ client/src/containers/Beers.js
@@ -2,30 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../App.css';
 import BeerCard from '../components/BeerCard';
-import { removeBeer, upvoteBeer, downvoteBeer } from '../actions/beerActions';
+import { removeBeer, upvoteBeer, downvoteBeer, getBeers } from '../actions/beerActions';
+import { Route } from 'react-router-dom';
+import DisplayBeer from './DisplayBeer';
+import BeerInput from './BeerInput';
+
 
 class Beers extends Component {
 
   render() {
-    const { beers, removeBeer, upvoteBeer, downvoteBeer } = this.props;
+    const beers = this.props.beers
 
     return (
-      <div className="BeersContainer">
-        <h1 className="beerName">Beers</h1>
-          {beers.map(beer =>
-          <BeerCard
-          key={beer.id}
-          upvoteBeer={upvoteBeer}
-          downvoteBeer={downvoteBeer}
-          removeBeer={removeBeer}
-          beer={beer} /> )}
+      <div>
+        <BeerInput />
+            <div className="BeersContainer">
+              <h1 className="beerName">Beers</h1>
+                {beers.map(beer =>
+                <BeerCard
+                key={beer.id}
+                upvoteBeer={upvoteBeer}
+                downvoteBeer={downvoteBeer}
+                removeBeer={removeBeer}
+                beer={beer} /> )}
+          </div>
       </div>
     );
   }
 };
 
 const mapStateToProps = state => {
-  return { beers: state.beers }
+  return { beers: state.beersReducer }
 }
 
-export default connect(mapStateToProps, {upvoteBeer, removeBeer, downvoteBeer})(Beers);
+export default connect(mapStateToProps, {upvoteBeer, removeBeer, downvoteBeer, getBeers})(Beers);

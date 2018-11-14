1d628a1465545a739320c1f3d9ec474a00bc6f5e
Michele Conway <mconway890@gmail.com>
2018-11-14 12:25:01 -0500

    About page edit

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,7 +4,8 @@ const About = () => {
   return(
     <div>
       <h1>About</h1>
-      <p>Beer Fridge is an app for beer lovers that want to keep track of what they've tried.</p>
+      <p>Beer Fridge is an app that allows you to share the beers you're currently enjoying.</p>
+      <img src="https://images.unsplash.com/photo-1535359056830-d4badde79747?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8da072ce4ba539795511d7493d0373be&auto=format&fit=crop&w=1404&q=80" width="500px"/>
     </div>
   )
 }

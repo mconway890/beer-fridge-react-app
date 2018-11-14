eaac44ec211a594e5e042b16c368cd4203944112
Michele Conway <mconway890@gmail.com>
2018-11-14 15:33:12 -0500

    Add description to beers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_150442) do
+ActiveRecord::Schema.define(version: 2018_11_14_190816) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_150442) do
     t.decimal "abv"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "breweries", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,54 +2,62 @@ Beer.create(
   name: "Astro Thunder",
   style: "IPA - Imperial/Double",
   brewery: "The Veil Brewing Co.",
-  abv: 8
+  abv: 8,
+  description: 'DIPA hopped equally with very fresh Enigma and Galaxy hops'
 )
 
 Beer.create(
   name: "Built For This",
   style: "IPA - American",
   brewery: "The Bronx Brewery",
-  abv: 7
+  abv: 7,
+  description: 'A cloudy, juicy IPA with lots of tropical fruit flavor'
 )
 
 Beer.create(
   name: "Peach Silhouette",
   style: "Sour - Ale",
   brewery: "Hudson Valley Brewery",
-  abv: 8
+  abv: 8,
+  description: 'Brunch-Style Sour IPA with peaches, and Mosaic & Citra hops'
 )
 
 Beer.create(
   name: 'Still Sound',
   style: 'IPA Imperial',
   brewery: 'Foam Brewers',
-  abv: 8
+  abv: 8,
+  description: 'Overipe mango, candied pineapple, dank, juice'
 )
 
 Beer.create(
   name: 'Horizon Loop',
   style: 'Sour Ale',
   brewery: 'Hudson Valley Brewery',
-  abv: 8
+  abv: 8,
+  description: 'Sour DIPA with raw wheat, malted oat, milk sugar, apricots & lemon myrtle; hopped w/ Simcoe powder'
 )
 
 Beer.create(
   name: 'King Julius',
   style: 'IPA Imperial Double',
   brewery: 'Tree House Brewing Company',
-  abv: 8.2
+  abv: 8.2,
+  description: 'King Julius is an American Double IPA brewed to be an exceptionally flavorful, juicy, and hop saturated beer while never tiring the palate. Its vivid citrus aromas give way to flavors of orange creamsicle, mango smoothie, and a bounty of fresh tropical fruit'
 )
 
 Beer.create(
   name: 'Fundamental Observation',
   style: 'Stout',
   brewery: 'Bottle Logic Brewing',
-  abv: 14.1
+  abv: 14.1,
+  description: 'Bourbon Barrel Aged Imperial Vanilla Stout'
 )
 
 Beer.create(
   name: 'Poppys Moonship',
   style: 'Sour Gose',
   brewery: 'Schilling Beer Company',
-  abv: 4.8
+  abv: 4.8,
+  description: 'A Gose brewed with coriander and sea salt. It features an assertive sourness delivered by lactobacillus'
 )

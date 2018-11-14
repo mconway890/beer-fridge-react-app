748f7f96be65cc66511cd7734a24cd1a291af1f1
Michele Conway <mconway890@gmail.com>
2018-11-14 15:40:11 -0500

    Change images on About and Contact

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
       <h1>About</h1>
       <p>Beer Fridge is an app that allows you to share the beers you're currently enjoying.</p>
-      <img src="https://images.unsplash.com/photo-1535359056830-d4badde79747?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8da072ce4ba539795511d7493d0373be&auto=format&fit=crop&w=1404&q=80" alt="beer" width="500px"/>
+      <img src="https://images.unsplash.com/photo-1524587253133-266042ff67d0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3a7f2d6e1d18dda9c3a681824bd5c2c7&auto=format&fit=crop&w=800&q=60" alt="beer" width="500px"/>
     </div>
   )
 }
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,6 +5,7 @@ const Contact = () => {
     <div>
       <h1>Contact</h1>
       <p>For questions contact us at beerfridge@gmail.com</p>
+      <img src="https://images.unsplash.com/photo-1465515779535-63e31c612e53?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4a79613ab5c9ee06d44c8837bf0fe4a&auto=format&fit=crop&w=1650&q=80" width="500px" alt="payphone"/>
     </div>
   )
 }

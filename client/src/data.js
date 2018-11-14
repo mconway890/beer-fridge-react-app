a4c7ccb63211ca20c1015632e44fdd25e09ad5ba
Michele Conway <mconway890@gmail.com>
2018-11-14 10:43:22 -0500

    Edit data

--- client/src/data.js
+++ client/src/data.js
@@ -1,26 +1,36 @@
-let beers = [
+let beerss = [
   {
     name: 'Still Sound',
     style: 'IPA Imperial',
-    brewery: 'Foam Brewers'
+    brewery: 'Foam Brewers',
+    abv: 8
   },
   {
     name: 'Horizon Loop',
     style: 'Sour Ale',
-    brewery: 'Hudson Valley Brewery'
-  }
-]
-
-let breweries = [
-  {
-    name: 'Foam Brewers'
+    brewery: 'Hudson Valley Brewery',
+    abv: 8
   },
   {
-    name: 'Hudson Valley'
-  }
+    name: 'King Julius',
+    style: 'IPA Imperial Double',
+    brewery: 'Tree House Brewing Company',
+    abv: 8.2
+  },
+  {
+    name: 'Fundamental Observation',
+    style: 'Stout',
+    brewery: 'Bottle Logic Brewing',
+    abv: 14.1
+  },
+  {
+    name: 'Poppys Moonship',
+    style: 'Sour Gose',
+    brewery: 'Schilling Beer Company',
+    abv: 4.8
+  },
 ]
 
 module.exports = {
-  beers,
-  breweries
+  beers
 }

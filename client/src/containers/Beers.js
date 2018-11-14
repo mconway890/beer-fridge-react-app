9ee7c090756529acee7131173f3dd5dbb450dbce
Michele Conway <mconway890@gmail.com>
2018-11-14 11:48:00 -0500

    Beer containers renders beer card table

--- client/src/containers/Beers.js
+++ client/src/containers/Beers.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../App.css';
-import { removeBeer, upvoteBeer, downvoteBeer, getBeers } from '../actions/beerActions';
-import CsvToHtmlTable from '../components/csvToHtmlTable.jsx';
-import { csvData } from "../csvData";
-
+import BeerCard from '../components/BeerCard';
+import { deleteBeer, upvoteBeer, downvoteBeer, getBeers } from '../actions/beerActions';
+import {Table} from 'react-bootstrap';
 
 class Beers extends Component {
 
   render() {
+    //const beers = this.props
+
     return (
       <div>
-        <CsvToHtmlTable
-        data={csvData}
-        csvDelimiter=","
-        tableClassName="table table-striped table-hover"
-        />
+        <div className="BeersContainer">
+          <h1 className="beerName">Beers</h1>
+          <div>
+            <Table striped bordered condensed hover>
+            <thead>
+              <tr>
+                <th>Beer Name</th>
+                <th>Style</th>
+                <th>ABV</th>
+                <th>Brewery</th>
+              </tr>
+            </thead>
+
+            {this.props && this.props.beers.map(beer =>
+            <BeerCard
+            key={beer.id}
+            upvoteBeer={upvoteBeer}
+            downvoteBeer={downvoteBeer}
+            deleteBeer={deleteBeer}
+            beer={beer}
+             /> )}
+            </Table>
+          </div>
+        </div>
       </div>
     );
   }
@@ -25,4 +45,4 @@ const mapStateToProps = state => {
   return { beers: state.beersReducer }
 }
 
-export default connect(mapStateToProps, {upvoteBeer, removeBeer, downvoteBeer, getBeers})(Beers);
+export default connect(mapStateToProps, {upvoteBeer, deleteBeer, downvoteBeer, getBeers})(Beers);

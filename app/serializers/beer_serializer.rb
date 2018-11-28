35a6a86cfbc923b52b56ded58a0931e3a18e8431
Michele Conway <mconway890@gmail.com>
2018-11-28 10:36:12 -0500

    Add comments for reference

--- app/serializers/beer_serializer.rb
+++ app/serializers/beer_serializer.rb
@@ -1,3 +1,4 @@
 class BeerSerializer < ActiveModel::Serializer
   attributes :id, :name, :style, :abv, :brewery, :description, :votes
+  # attributes required by the client (the params for the class instance)
 end

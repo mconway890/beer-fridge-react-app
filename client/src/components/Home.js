9df19bb84058ebdcf0ed6a9bf178e07a519554db
Michele Conway <mconway890@gmail.com>
2018-11-28 11:01:17 -0500

    Add comments for reference, Change stateless components to functions

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css'
 import {Image} from 'react-bootstrap';
 
+// component without state doesn't need to be a JS object of type Component
 const Home = () => {
   return (
     <div>

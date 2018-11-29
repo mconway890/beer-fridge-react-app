624e695bdfeccb5aa1ba5f6bfda0695817236e4e
Michele Conway <mconway890@gmail.com>
2018-11-29 12:52:29 -0500

    Change href to Link

--- client/src/components/BreweryCard.js
+++ client/src/components/BreweryCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import DeleteBrewery from '../containers/DeleteBrewery';
 import UpvoteBrewery from '../containers/UpvoteBrewery';
 import DownvoteBrewery from '../containers/DownvoteBrewery';
+import {Link} from 'react-router-dom';
 
 const BreweryCard = ({brewery, likeBrewery, dislikeBrewery, deleteBrewery}) =>
 			<tbody>
 				<tr>
-					<td><a href={`breweries/${brewery.id}`}>{brewery.name}</a></td>
+					<td><Link to={`breweries/${brewery.id}`}>{brewery.name}</Link></td>
 					<td><DeleteBrewery history={brewery.history} brewery={brewery}/></td>
 					<td><UpvoteBrewery history={brewery.history} brewery={brewery}/></td>
 					<td><DownvoteBrewery history={brewery.history} brewery={brewery}/></td>

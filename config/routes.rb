a139ed8b659100f07c86bfcde8490d4f99370ab3
Michele Conway <mconway890@gmail.com>
2018-12-04 15:39:14 -0500

    Add routes for increase and decrease

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
   	namespace :v1 do
-  		resources :beers
+  		resources :beers, :breweries, only: [:index, :show]
   	end
   end
 
-  namespace :api do
-  	namespace :v1 do
-  		resources :breweries
-  	end
-  end
+  patch '/api/v1/beers/:id/increase', to: 'api/v1/beers#increase'
+  patch '/api/v1/beers/:id/decrease', to: 'api/v1/beers#decrease'
+  patch '/api/v1/breweries/:id/increase', to: 'api/v1/breweries#increase'
 end
+
+# only route to index and show so update method isn't looked for in controller
+# update method replaced with increase and decrease
+# patch request made to increase route in order to update vote count by 1
+# patch request made to decrease route in order to update vote count down by 1
